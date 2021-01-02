08127850160480eb6d888997a16dce380c955970
rajksaha <raj.gmk@gmail.com>
2021-01-02 18:18:06 +0800

    removed error code

--- api/prescription.php
+++ api/prescription.php
@@ -3,9 +3,6 @@
 header("Content-Type:application/json");
 if (isset($_GET['appointmentID']) && $_GET['appointmentID']!="") {
 	include_once '../mpdf/default.php';
-	include_once '../config/database.php';
-	$db = new Database();
-	$conn = $db.getConnection();
 	$appointmentID = $_GET['appointmentID'];
 	$pdf = new PDF();
 	return $pdf.preparePrescription(db, $appointmentID);
